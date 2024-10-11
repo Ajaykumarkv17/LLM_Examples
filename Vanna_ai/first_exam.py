df605f594251c8fc2ab6e388e289654f63674ca5
Ajay Kumar K V <112631909+Ajaykumarkv17@users.noreply.github.com>
2024-10-11 18:05:44 +0000

    day-10

--- Vanna_ai/first_exam.py
+++
@@ -1,5 +0,0 @@
-import vanna
-from vanna.remote import VannaDefault
-vn = VannaDefault(model='chinook', api_key=vanna.get_api_key('my-email@example.com'))
-vn.connect_to_sqlite('https://vanna.ai/Chinook.sqlite')
-vn.ask("What are the top 10 albums by sales?")
